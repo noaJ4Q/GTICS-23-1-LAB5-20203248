c094d634a621e8c33919beffdbe6a6a7f1c3e71c
noe <noe.jaraq@gmail.com>
2023-04-25 20:43:36 -0500

    new files

---
+++ src/test/java/com/example/gtics231lab520203248/Gtics231Lab520203248ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520203248;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics231Lab520203248ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
