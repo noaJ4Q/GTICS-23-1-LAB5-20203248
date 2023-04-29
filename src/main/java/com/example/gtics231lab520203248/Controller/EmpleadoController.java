17b7f50daca931d560a6e3124fff2eabdeede4da
noe <noe.jaraq@gmail.com>
2023-04-29 11:57:54 -0500

    p2 lista

--- src/main/java/com/example/gtics231lab520203248/Controller/EmpleadoController.java
+++ src/main/java/com/example/gtics231lab520203248/Controller/EmpleadoController.java
@@ -8,11 +8,13 @@ import com.example.gtics231lab520203248.Repository.EmployeeRepository;
 import com.example.gtics231lab520203248.Repository.JobRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/Empleados", method = RequestMethod.GET)
@@ -33,11 +35,48 @@ public class EmpleadoController {
         return "empleados";
     }
 
-    @GetMapping("/Nuevo")
+    @GetMapping("/Buscar")
+    public String buscarEmpleado(@RequestParam(name = "busqueda") String busqueda,
+                                 Model model){
+        List<Employee> employeeList = employeeRepository.buscarPorVarios(busqueda);
+        model.addAttribute("employeeList", employeeList);
+        model.addAttribute("busqueda", busqueda);
+        return "empleados";
+    }
+
+    @GetMapping("/NuevoEmpleado")
     public String nuevo(Model model){
         List<Job> jobList = jobRepository.findAll();
         List<Department> departmentList = departmentRepository.findAll();
+        List<Employee> bossList = employeeRepository.buscarJefes();
         model.addAttribute("jobList", jobList);
+        model.addAttribute("departmentList", departmentList);
+        model.addAttribute("bossList", bossList);
         return "nuevoEmpleado";
+    }
+
+    @PostMapping("/Guardar")
+    public String guardar(Employee employee,
+                          RedirectAttributes attributes){
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        employee.setHire_date(format.format(date));
+        employeeRepository.save(employee);
+        attributes.addFlashAttribute("msgGuardado", "Empleado creado exitosamente");
+
+        return "redirect:/Empleados";
+    }
+
+    @GetMapping("/Borrar")
+    public String borrar(@RequestParam(name = "employee_id") int employee_id,
+                         RedirectAttributes attributes){
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employee_id);
+        if (optionalEmployee.isPresent()){
+
+            employeeRepository.deleteById(employee_id);
+            attributes.addFlashAttribute("msgBorrado", "Empleado borrado exitosamente");
+        }
+        return "redirect:/Empleados";
     }
 }
--- src/main/java/com/example/gtics231lab520203248/Repository/EmployeeRepository.java
+++ src/main/java/com/example/gtics231lab520203248/Repository/EmployeeRepository.java
@@ -2,8 +2,20 @@ package com.example.gtics231lab520203248.Repository;
 
 import com.example.gtics231lab520203248.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    @Query(nativeQuery = true, value = "select e.* from employees e " +
+            "inner join jobs j on (e.job_id = j.job_id) " +
+            "inner join departments d on (e.department_id = d.department_id) " +
+            "inner join locations l on (d.location_id = l.location_id) " +
+            "where e.first_name = ?1 or e.last_name = ?1 or j.job_title = ?1 or l.city = ?1")
+    List<Employee> buscarPorVarios (String busqueda);
+    @Query(nativeQuery = true, value = "select distinct m.* from employees e \n" +
+            "inner join employees m on (e.manager_id = m.employee_id)")
+    List<Employee> buscarJefes ();
 }
